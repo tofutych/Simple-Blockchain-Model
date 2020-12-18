c518a5d2a89dc5f58c2edf81635f882a9243a677
David <martirosov.davidka@mail.ru>
2020-12-18 23:58:14 +0300

    how to use it

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from blockchain import Blockchain
 
 if __name__ == "__main__":
     blockchain = Blockchain()
-    # blockchain.genesis_block()
+    blockchain.genesis_block()
 
-    # blockchain.from_csv("transactions.csv")
-    # blockchain.add_block(["A", "B", "6BTC"])
-    # blockchain.add_block(["B", "C", "7BTC"])
+    blockchain.from_csv("transactions.csv")
+    blockchain.add_block(["A", "B", "6BTC"])
+    blockchain.add_block(["B", "C", "7BTC"])
 
     blockchain.check_integrity()
